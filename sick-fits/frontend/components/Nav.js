abe2ceba3070366680881880a8fb117e00b5c647
Jonny Nabors <jonnynabors@gmail.com>
2018-11-27 10:43:02 -0500

    Use styled components for better ux

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,15 +1,15 @@
 import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
 
 const Nav = () => (
-    <div>
-        <Link href='/sell'>
-            <a>Sell</a>
-        </Link>
-        <Link href="/">
-            <a>Home</a>
-        </Link>
+    <NavStyles>
+        <Link href="/items"><a>items</a></Link>
+        <Link href="/sell"><a>sell</a></Link>
+        <Link href="/signup"><a>signup</a></Link>
+        <Link href="/orders"><a>orders</a></Link>
+        <Link href="/me"><a>account</a></Link>
 
-    </div>
+    </NavStyles>
 )
 
 export default Nav;
